8a9d41c1eeae28ed905e44d38ec2b056cfc6b682
Louis Frayser (as android) <louis.frayser@gmail.com>
2024-10-07 21:36:00 -0700

    Moved db code to schema.scm

--- apps/PlanC/schema.scm
+++ apps/PlanC/schema.scm
@@ -31,6 +31,62 @@
    (list-index
     (lambda (s) (string=? s *category ) ) *categories)))
 
+;;; =================================================================
+;;; -------------- Database ----------------------------------------
+;;; The on-disk database
+;;; Verify an open db with tables created
+(define *_db #f)
+(define (get-db)
+  (unless *_db
+    (set! *_db (sqlite-open %dbpath))
+    (let* ((sql "SELECT name FROM sqlite_master
+               WHERE type='table' AND name='assocs';")
+	   (tabs (sqlite-query *_db sql)))
+      (when (null-list? tabs)
+	(sqlite-query
+	 *_db
+	 (string-append
+	  "CREATE TABLE assocs("
+	  " ctime timestamp,"
+	  " category varchar(25),"
+	  " activity varchar (25),"
+	  " stime timestamp,"
+	  " duration timestamp,"
+	  " usr varchar (25)"
+	  ");")))))
+    *_db)
+
+(define (dbstore ctime cat act stm dur)
+  (define (qq s)( string-append "'" s  "', " ))
+  (let ((sql
+	 (string-append
+	  "INSERT INTO assocs"
+	  " VALUES ("
+	  (apply string-append (map qq (list ctime  cat  act  stm dur )))
+	  "'" %user% "' );")))
+	(sqlite-query (get-db) sql)))
+
+;;; Read the entire database
+(define (get-assocs) ; Return list of lists
+  (sqlite-query (get-db)
+		 "SELECT ctime, category, activity,stime, duration \
+                           FROM assocs \
+
+                          ORDER by stime desc;"))
+
+(define (db-get-history-lines)
+  ;; Data for a list widget.  Hope it has a monspace font.
+  ;; ctime,cat,act,stime,dur -> stime,activity,duration
+  ;; Provides stime,activity,duration
+  (let* ((raw-data (map cddr (get-assocs)))
+	 (->s-a-d (lambda (r)(list (setw (cadr r) 20) (setw (car r) 24) (caddr r) )))
+	 (rows (map ->s-a-d raw-data))
+	 ;;(rows raw-data)
+	 ;;(sw (lambda(s)(setw s 16))) ; Width of a date-time field is 16
+	 (row2line (lambda(r)(string-mapconcat r " " )))
+	 (lines (map row2line rows)))
+    lines ))
+
 (define (db-get-last)
   ;; Get the last record or #f
   (let ((recset
